a323392aad253a3e50fb107dd08cda2124a65a48
MedQR <medqrchile@gmail.com>
2025-07-18 20:06:52 -0400

    Update adminpanel.jsx

--- src/pages/adminpanel.jsx
+++ src/pages/adminpanel.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { db } from '../firebase';
+import { db } from './firebase';
 import { collection, getDocs } from 'firebase/firestore';
 import QRCode from 'qrcode.react';
 import logoImage from '../assets/logo.png'; // Importación al inicio
